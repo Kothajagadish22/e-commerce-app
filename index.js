7e561c63a3297e6edd1b740eaae7ece637ce7725
Kothajagadish22 <19pa1a0345@vishnu.edu.in>
2023-01-17 17:48:53 +0530

    Added express to application

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hi");
+const express = require("express");
+const app = express();
+const port = 3000;
+app.get("/", (req, res) => {
+  res.send("Good morning");
+});
+app.get("/movie", (req, res) => {
+  res.send("RRR and valteir verraya is famous");
+});
+app.listen(port, () => {
+  console.log("server is running on 3000");
+});
